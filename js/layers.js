b6ccb0221e47793dd4b48ab666c7383dd84a9492
Atomic23982 <spacedino.bro@gmail.com>
2022-07-05 18:27:25 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,10 +6,10 @@ addLayer("OW", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#BF40BF", 
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Overwrites", // Name of prestige currency
+    baseResource: "Resets", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -22,7 +22,7 @@ addLayer("OW", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "Shift+O", description: "Shift+O: Reset for Overwrites", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
