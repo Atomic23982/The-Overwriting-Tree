a5efe5dfcdec6ec3560d6176bcb8537e4968d652
Atomic23982 <spacedino.bro@gmail.com>
2022-07-06 12:57:21 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("OW", {
     baseResource: "Resets", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.75,
+    exponent: 0.5,
     passiveGeneration() { if (hasUpgrade('OW', 21)) return 0.05 },
     gainMult() {
         let mult = new Decimal(1)
