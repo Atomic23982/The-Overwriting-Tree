48ee971be708cb615e36bbe839853253df8b62d1
Atomic23982 <spacedino.bro@gmail.com>
2022-07-06 14:08:20 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,7 +24,7 @@ addLayer("OW", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "Shift+o", description: "Shift+o: Reset for Overwrites", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "O", description: "O: Reset for Overwrites", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}, upgrades: {
         11: {
@@ -86,3 +86,30 @@ addLayer("OW", {
         },
     },
 })
+
+addLayer("OB", {
+    name: "Overwrite Boosters", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "OB", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+    }},
+    color: "#BF40BF", 
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Overwrites", // Name of prestige currency
+    baseResource: "Resets", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5,
+    gainMult() {
+        let mult = new Decimal(1)
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "", description: "Shift+o: Reset for Overwrites", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+})    
