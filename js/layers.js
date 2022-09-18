d8636e5c35bd602b2b9aa99a001eb3e65524ffed
Atomic23982 <spacedino.bro@gmail.com>
2022-09-17 19:08:10 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -13,7 +13,7 @@ addLayer("OW", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5,
-    passiveGeneration() { if (hasUpgrade('OW', 22)) return 0.05 },
+    passiveGeneration() { if (hasUpgrade('OW', 22)) return 0.75 },
     gainMult() {
         let mult = new Decimal(1)
         if (hasUpgrade('OW', 23)) mult = mult.times(upgradeEffect('OW', 23))
@@ -99,8 +99,8 @@ addLayer("TO", {
     }},
     color: "#BF40BF", 
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Overwrites", // Name of prestige currency
-    baseResource: "Resets", // Name of resource prestige is based on
+    resource: "Time Overwrite", // Name of prestige currency
+    baseResource: "Overwrites", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5,
@@ -111,7 +111,7 @@ addLayer("TO", {
 gainExp() { // Calculate the exponent on main currency from bonuses
     return new Decimal(1)
 },
-row: 0, // Row the layer is in on the tree (0 is the first row)
+row: 1, // Row the layer is in on the tree (0 is the first row)
 hotkeys: [
     {key: "T", description: "T: Reset for Time Overwrites", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
 ],
